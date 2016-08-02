defc0cc74723abf1f13560dcf5e420fc83a5671c
Koen Schmeets <k.schmeets@gmail.com>
2016-08-02 17:17:20 +0200

    Add scripts

---
+++ apps/mobile/scripts/download-tiles-mbtiles.js
@@ -0,0 +1,95 @@
+var fs = require('fs')
+var path = require('path')
+var zlib = require('zlib')
+var mbtiles = require('mbtiles')
+var through = require('through')
+var mkdirp = require('mkdirp');
+var geojsonToTiles = require('./geojson-to-tiles')
+
+// var maxZoom = 14;
+// var pattern = 'features.*.geometry.coordinates.*'
+// var mbtilesPath = '../ios/www/track1.mbtiles'
+// var outputDir = '../ios/www/track1'
+// var tilesURl = 'http://localhost:9997/' + path.basename(outputDir) + '/{z}/{x}/{y}.pbf'
+
+var maxZoom = 14;
+var pattern = 'features.*.geometry.coordinates'
+var mbtilesPath = '../ios/www/camps.mbtiles'
+var outputDir = '../ios/www/camps'
+var tilesURl = 'http://localhost:9997/' + path.basename(outputDir) + '/{z}/{x}/{y}.pbf'
+
+function fixTileJSONCenter(tileJSON) {
+  if (tileJSON.bounds && !tileJSON.center) {
+    var fitWidth = 1024;
+    var tiles = fitWidth / 256;
+    tileJSON.center = [
+      (tileJSON.bounds[0] + tileJSON.bounds[2]) / 2,
+      (tileJSON.bounds[1] + tileJSON.bounds[3]) / 2,
+      Math.round(
+        -Math.log((tileJSON.bounds[2] - tileJSON.bounds[0]) / 360 / tiles) /
+        Math.LN2
+      )
+    ];
+  }
+};
+
+var tileJSON = {}
+var mbtilesFile = path.join(mbtilesPath);
+var id = path.basename(mbtilesPath)
+var source = new mbtiles(mbtilesFile, function(err) {
+  source.getInfo(function(err, info) {
+    tileJSON['name'] = id;
+    tileJSON['format'] = 'pbf';
+    Object.assign(tileJSON, info);
+    tileJSON['tilejson'] = '2.0.0';
+    tileJSON['basename'] = id;
+    tileJSON['filesize'] = fs.statSync(mbtilesFile)['size'];
+    delete tileJSON['scheme'];
+    fixTileJSONCenter(tileJSON);
+  });
+
+  geojsonToTiles(process.stdin, maxZoom, pattern)
+    .pipe(through(function (tile) {
+      var parts = tile.split('/')
+      var z = parts[0]
+      var x = parts[1]
+      var y = parts[2]
+      var streamSelf = this;
+      var outputFile = outputDir + '/' + tile + '.pbf'
+      if (fs.existsSync(outputFile)) {
+        return
+      }
+      streamSelf.pause()
+      source.getTile(z, x, y, function(err, data, headers) {
+        if (err) {
+          // console.error(err)
+          streamSelf.resume()
+          return
+        }
+        if (data == null) {
+          // console.error('Not found')
+          streamSelf.resume()
+          return
+        }
+        zlib.gunzip(data, function (err, ungzipped) {
+          if (err) {
+            // console.error(err)
+            streamSelf.resume()
+            return
+          }
+          mkdirp(path.dirname(outputFile), function () {
+            fs.writeFile(outputFile, ungzipped, function () {
+              streamSelf.resume()
+            })
+          })
+        })
+      });
+    }))
+    .on('end', function () {
+      tileJSON.tiles = [
+        tilesURl
+      ]
+      fs.writeFileSync(outputDir + '/metadata.json', JSON.stringify(tileJSON, undefined, 2))
+      console.log('Done')
+    })
+});
---
+++ apps/mobile/scripts/download-tiles.js
@@ -0,0 +1,63 @@
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var through = require('through');
+var request = require('request');
+var zlib = require('zlib');
+var geojsonToTiles = require('./geojson-to-tiles')
+
+// var maxZoom = Number(process.env.MAXZOOM);
+// // var token = process.env.MAPBOX_ACCESS_TOKEN;
+// // var url = 'https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6';
+// // var ext = '.vector.pbf?access_token=' + token;
+// var outputDir = process.env.OUTPUT_DIR;
+// var outputExt = process.env.OUTPUT_EXT || '.pbf';
+// var url = process.env.TILES_URL ? process.env.TILES_URL : 'https://tiles.dwaler.com/data/osm2vectortiles';
+
+var maxZoom = 14;
+var pattern = 'features.*.geometry.coordinates.*'
+var outputDir = '../ios/www/map'
+// var url = 'https://tiles.dwaler.com/data/osm2vectortiles'
+var url = 'http://osm2vectortiles-3.tileserver.com/v2'
+var ext = '.pbf'
+var tilesURl = 'http://localhost:9997/' + path.basename(outputDir) + '/{z}/{x}/{y}.pbf'
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+geojsonToTiles(process.stdin, maxZoom, pattern)
+  .pipe(through(function (tile) {
+    var parts = tile.split('/')
+    var z = parts[0]
+    var x = parts[1]
+    var y = parts[2]
+    var streamSelf = this;
+    var dlUrl = url + '/' + z + '/' + x + '/' + y + ext
+    var outputFile = outputDir + '/' + tile + '.pbf'
+    if (fs.existsSync(outputFile)) {
+      return
+    }
+    streamSelf.pause()
+    request.get(dlUrl, {encoding: null}, function (err, resp, body) {
+      if (err) {
+        console.log(err)
+        streamSelf.resume()
+        return
+      }
+      if (body === 'Tile does not exist') {
+        streamSelf.resume()
+        return
+      }
+      zlib.gunzip(body, function (err, ungzipped) {
+        mkdirp(path.dirname(outputFile), function () {
+          fs.writeFile(outputFile, ungzipped, function () {
+            setTimeout(function () {
+              streamSelf.resume()
+            }, 20)
+          })
+        })
+      })
+    });
+  }))
+  .on('end', function () {
+    console.log('Done')
+  })
