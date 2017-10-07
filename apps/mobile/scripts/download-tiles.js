b308a293c60b69cba455bf548da334cf562401d8
Koen Schmeets <k.schmeets@gmail.com>
2017-10-07 10:51:01 +0200

    Resolve more conflicts

--- apps/mobile/scripts/download-tiles.js
+++ apps/mobile/scripts/download-tiles.js
@@ -8,11 +8,8 @@ var geojsonToTiles = require('./geojson-to-tiles')
 // var maxZoom = Number(process.env.MAXZOOM);
 var token = process.env.MAPBOX_ACCESS_TOKEN;
 // var url = 'https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6';
-<<<<<<< HEAD
-=======
 var url = 'https://b.tiles.mapbox.com/v4/mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v7';
 var ext = '.vector.pbf?access_token=' + token;
->>>>>>> dev
 // var outputDir = process.env.OUTPUT_DIR;
 var outputExt = process.env.OUTPUT_EXT || '.pbf';
 // var url = process.env.TILES_URL ? process.env.TILES_URL : 'https://tiles.dwaler.com/data/osm2vectortiles';
@@ -22,18 +19,10 @@ var pattern = 'features.*.geometry.coordinates.*'
 var outputDir = '../ios/www/map'
 // var url = 'https://tiles.dwaler.com/data/osm2vectortiles'
 // var url = 'http://osm2vectortiles-0.tileserver.com/v2'
-<<<<<<< HEAD
-// var ext = '.pbf'
-var url = 'https://b.tiles.mapbox.com/v4/mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v7'
-var ext = '.vector.pbf?access_token=' + token;
 
-// var url = 'https://free-3.tilehosting.com/data/v3'
-// var ext = '.pbf.pict?key=RiS4gsgZPZqeeMlIyxFo'
-=======
 // var url = 'http://osm2vectortiles-0.tileserver.com/v3'
 // var url = 'http://osm2vectortiles-1.tileserver.com/v3'
 // var ext = '.pbf'
->>>>>>> dev
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -50,27 +39,6 @@ geojsonToTiles(process.stdin, maxZoom, pattern, 'neighbours')
     if (fs.existsSync(outputFile)) {
       return
     }
-<<<<<<< HEAD
-    streamSelf.pause()
-    request.get(dlUrl, {encoding: null}, function (err, resp, body) {
-      if (err) {
-        console.log(err)
-        setTimeout(() => {
-          streamSelf.resume()
-        }, 5000)
-        return
-      }
-      if (body === 'Tile does not exist') {
-        streamSelf.resume()
-        return
-      }
-      mkdirp(path.dirname(outputFile), function () {
-        fs.writeFile(outputFile, body, function () {
-          setTimeout(function () {
-            streamSelf.resume()
-          }, 150)
-        })
-=======
     // streamSelf.pause()
     downloadChain = downloadChain.then(() => {
       return new Promise(resolve => {
@@ -93,7 +61,6 @@ geojsonToTiles(process.stdin, maxZoom, pattern, 'neighbours')
             })
           })
         });
->>>>>>> dev
       })
     })
   }))
